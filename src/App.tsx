e89b9e829ae6ae812816d65d58791cd907d481d3
Vlad Harkusha <sivari888@gmail.com>
2022-11-30 13:21:52 +0200

    add react routes and page folders, add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
+import { AccessoriesPage } from './components/AccessoriesPage';
+import { Header } from './components/Header';
+import { HomePage } from './components/HomePage';
+import { PhonesPage } from './components/PhonesPage';
+import { TabletsPage } from './components/TabletsPage';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Product Catalog App with Eslint and Husky</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate to='/product_catalog_front/home' replace />}
+          />
+          <Route
+            path="/product_catalog_front/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/product_catalog_front/home"
+            element={<HomePage />}
+          />
+          <Route
+            path="/product_catalog_front/phones"
+            element={<PhonesPage />}
+          />
+          <Route
+            path="/product_catalog_front/tablets"
+            element={<TabletsPage />}
+          />
+          <Route
+            path="/product_catalog_front/accessories"
+            element={<AccessoriesPage />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 

6f9f900cdda5fd4cfa4baf56927032e5fe6b88c7
Vlad Harkusha <sivari888@gmail.com>
2022-12-01 23:01:58 +0200

    fix link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,7 +19,8 @@ export const Footer: React.FC = () => {
             <a
               href="https://github.com/fe-aug22-blackout-team"
               className="footer__link footer__github"
-              target="blanc"
+              rel="noreferrer"
+              target="_blank"
             >
               Github
             </a>

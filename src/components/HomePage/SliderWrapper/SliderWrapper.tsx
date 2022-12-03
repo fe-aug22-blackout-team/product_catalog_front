e88637fc1d649336c8f77c9bdb0bc4c823c5187a
Vlad Harkusha <sivari888@gmail.com>
2022-12-03 16:28:30 +0200

    slider version 1.0

--- src/components/HomePage/SliderWrapper/SliderWrapper.tsx
+++ src/components/HomePage/SliderWrapper/SliderWrapper.tsx
@@ -6,22 +6,21 @@ import './SliderWrapper.scss';
 
 export const SliderWrapper: React.FC = () => {
   return (
-    <Slider>
-      <SliderItem>
-        <div className="slider-item__1"></div>
-      </SliderItem>
-
-      <SliderItem>
-        <div className="slider-item__2"></div>
-      </SliderItem>
-
-      <SliderItem>
-        <div className="slider-item__3"></div>
-      </SliderItem>
-
-      <SliderItem>
-        <div className="slider-item__4"></div>
-      </SliderItem>
-    </Slider>
+    <div className="sliderWrapper">
+      <Slider>
+        <SliderItem>
+          <div className="slider-item__1"></div>
+        </SliderItem>
+        <SliderItem>
+          <div className="slider-item__2"></div>
+        </SliderItem>
+        <SliderItem>
+          <div className="slider-item__3"></div>
+        </SliderItem>
+        <SliderItem>
+          <div className="slider-item__4"></div>
+        </SliderItem>
+      </Slider>
+    </div>
   );
 };

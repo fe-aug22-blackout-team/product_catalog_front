17410d6c5218c45f0cd565081fb5e12417724c91
Vadym Mamchur <vmchr344@gmail.com>
2022-12-05 17:12:13 +0200

    /phone`s link has been updated

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-const API_URL = 'https://638b585b0bb60929b532cc46--melodic-centaur-2d31fb.netlify.app/.netlify/functions/server';
+const API_URL = 'https://melodic-centaur-2d31fb.netlify.app/.netlify/functions/server';
 
 export async function getPhonesByPagination(
   sortBy: string,

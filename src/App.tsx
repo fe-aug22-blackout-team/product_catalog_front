c3d4b67dc415ed1b6b08f5362d94a7259bf6bbdc
Vlad Harkusha <sivari888@gmail.com>
2022-12-06 18:27:09 +0200

    add cart and favourits count to burgerMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.scss';
 import { AccessoriesPage } from './components/AccessoriesPage';

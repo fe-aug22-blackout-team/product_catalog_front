b67d5e1b9b04bee03d01a7d6ec1ce846376a4294
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-12-06 11:46:24 +0200

    Remove ProductInfo component from Tablets page

--- src/components/TabletsPage/TabletsPage.tsx
+++ src/components/TabletsPage/TabletsPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { ProductInfo } from '../ProductInfo';
 import './TabletsPage.scss';
 
 export const TabletsPage: React.FC = () => {
   return (
-    // <h1 className="title">TabletsPage is under development</h1>
-    <ProductInfo />
+    <h1 className="title">TabletsPage is under development</h1>
   );
 };

ab174243b66639c7bdf7a73519ccc63370f92011
Vlad Harkusha <sivari888@gmail.com>
2022-12-08 16:52:32 +0200

    route fixes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,9 +1,9 @@
 export const appRoutes = {
-  home: 'product_catalog_front/',
-  phones: 'product_catalog_front/phones',
-  tablets: 'product_catalog_front/tablets',
-  accessories: 'product_catalog_front/accessories',
-  favourites: 'product_catalog_front/favourites',
-  cart: 'product_catalog_front/cart',
-  not_found: 'product_catalog_front/not_found',
+  home: '/product_catalog_front/',
+  phones: '/product_catalog_front/phones',
+  tablets: '/product_catalog_front/tablets',
+  accessories: '/product_catalog_front/accessories',
+  favourites: '/product_catalog_front/favourites',
+  cart: '/product_catalog_front/cart',
+  not_found: '/product_catalog_front/not_found',
 };
